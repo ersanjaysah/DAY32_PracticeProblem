91d1f26b6893b53dfed0b648ebc4ff034c3f53d4
sanjay <ssah6694@gmail.com>
2022-04-23 09:42:01 +0530

    [sanjay]Added: welcome file

---
+++ SQL_ER_DIAG/SQL_ER_DIAG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQL_ER_DIAG
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the SQL ER Diagram");
+        }
+    }
+}
